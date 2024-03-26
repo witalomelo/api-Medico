bf9c61d7beb79409715d45f3cbce75d53e642710
witalomelo <witalovmelo@gmail.com>
2024-03-26 08:07:10 -0300

    Trazendo apenas paciente com status ativo

--- api/src/main/java/med/voll/api/paciente/Paciente.java
+++ api/src/main/java/med/voll/api/paciente/Paciente.java
@@ -21,8 +21,10 @@ public class Paciente {
     private String cpf;
     @Embedded
     private Endereco endereco;
+    private Boolean ativo;
 
     public Paciente(DadosCadastroPaciente dados) {
+        this.ativo = true;
         this.nome = dados.nome();
         this.email = dados.email();
         this.telefone = dados.telefone();
@@ -65,5 +67,10 @@ public class Paciente {
         if(dados.endereco() != null){
             this.endereco.atualizarInformacoes(dados.endereco());
         }
+    }
+
+    public void excluir() {
+        this.ativo = false;
+
     }
 }
