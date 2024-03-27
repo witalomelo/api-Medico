5d11ad892a20371cd50581f43a16320d8622bdec
witalomelo <witalovmelo@gmail.com>
2024-03-27 08:59:35 -0300

    Personalizando erros na API - stacktrace

--- api/src/main/java/med/voll/api/controller/PacienteController.java
+++ api/src/main/java/med/voll/api/controller/PacienteController.java
@@ -1,7 +1,6 @@
 package med.voll.api.controller;
 
 import jakarta.validation.Valid;
-import med.voll.api.medico.DadosDetalhamentoMedico;
 import med.voll.api.paciente.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
