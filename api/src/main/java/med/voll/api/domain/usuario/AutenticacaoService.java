0da7cae90d8fc6d91a4109579f34ed46be7230c1
witalomelo <witalovmelo@gmail.com>
2024-03-29 10:40:29 -0300

    Configuracoes de seguranca

--- api/src/main/java/med/voll/api/domain/usuario/AutenticacaoService.java
+++ api/src/main/java/med/voll/api/domain/usuario/AutenticacaoService.java
@@ -7,7 +7,7 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
-public class AutenticacaoService implements UserDetailsService {
+public class AutenticacaoService implements UserDetailsService { //interface que define método para carregar detalhes de um usuário por meio do nome
 
     @Autowired
     private UsuarioRepository repository;
--- api/src/main/java/med/voll/api/domain/usuario/UsuarioRepository.java
+++ api/src/main/java/med/voll/api/domain/usuario/UsuarioRepository.java
@@ -4,5 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-    UserDetails findByLogin(String login); //metodo que vai fazer a consulta no BD
+    UserDetails findByLogin(String login); //metodo que vai fazer a consulta no DB
 }
